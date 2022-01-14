9e52c13cf7e0ed62a62051be0d1e8466b1ec1681
bnguy118 <bnguy118@ucsc.edu>
2022-01-14 14:51:14 -0800

    updating files to have header comments

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -1,53 +1,60 @@
+/*********************************************************************************
+* Brian Nguyen, bnguy118
+* 2022 Winter CSE101 PA1
+* Lex.c
+* Client function to alphabetize the lines in a file using List ADT
+*********************************************************************************/
 #include "List.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 #define MAX_LEN 300
-int main(int argc, char *argv[])  {
+int main(int argc, char* argv[])
+{
 	//Initialize variables used for program and command-line parsing
 	FILE *input_file, *output_file;
 	char buffer[MAX_LEN];
 	char newline_checker;
 	int line_amount = 0;
-	char **strings;
-	
+	char** strings;
+
 	//Give respective outputs based on parsed command line input
-	if(argc != 3) {
-      		printf("Usage: %s <input file> <output file>\n", argv[0]);
-      		exit(EXIT_FAILURE);
+	if (argc != 3) {
+		printf("Usage: %s <input file> <output file>\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
-	
+
 	input_file = fopen(argv[1], "r");
-	if(input_file == NULL) {
-      		printf("Unable to open file %s for reading\n", argv[1]);
-      		exit(EXIT_FAILURE);
-   	}
-	
+	if (input_file == NULL) {
+		printf("Unable to open file %s for reading\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
 	output_file = fopen(argv[2], "w");
-   	if(output_file == NULL) {
-      		printf("Unable to open file %s for writing\n", argv[2]);
-      		exit(EXIT_FAILURE);
-   	}
+	if (output_file == NULL) {
+		printf("Unable to open file %s for writing\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
 
 	//Count total lines of input file
-	while((newline_checker = fgetc(input_file)) != EOF) {
-		if(newline_checker == '\n') {
+	while ((newline_checker = fgetc(input_file)) != EOF) {
+		if (newline_checker == '\n') {
 			line_amount += 1;
 		}
 	}
-	
+
 	//Allocate string array and add strings to string array
-	strings = (char **) calloc(line_amount, sizeof(char *));
+	strings = (char**)calloc(line_amount, sizeof(char*));
 	rewind(input_file);
 	int i = 0;
-	while(fgets(buffer, MAX_LEN, input_file) != NULL) {
+	while (fgets(buffer, MAX_LEN, input_file) != NULL) {
 		//Removes trailing newline
 		buffer[strcspn(buffer, "\n")] = 0;
-		
+
 		//Allocates memory for each scanned string
-		strings[i] = malloc((strlen(buffer) + 1) * sizeof(char *));
+		strings[i] = malloc((strlen(buffer) + 1) * sizeof(char*));
 		strcpy(strings[i], buffer);
 		i += 1;
 	}
@@ -55,29 +62,28 @@ int main(int argc, char *argv[])  {
 	//Indirectly sort array by adding indices to L with insertion sort algorithm
 	List L = newList();
 	prepend(L, 0);
-	for(int i = 1; i < line_amount; i++) {
+	for (int i = 1; i < line_amount; i++) {
 		moveBack(L);
-		while((index(L) >= 0) && (strcmp(strings[i], strings[get(L)]) < 0)) {
+		while ((index(L) >= 0) && (strcmp(strings[i], strings[get(L)]) < 0)) {
 			movePrev(L);
 		}
-		if(index(L) == -1) {
+		if (index(L) == -1) {
 			prepend(L, i);
-		}
-		else {
+		} else {
 			insertAfter(L, i);
 		}
 	}
-	
+
 	//Print array in lexicographic order
-	for(moveFront(L); index(L) < length(L); moveNext(L)) {
+	for (moveFront(L); index(L) < length(L); moveNext(L)) {
 		fprintf(output_file, "%s\n", strings[get(L)]);
-		if(index(L) == (length(L) - 1)) {
+		if (index(L) == (length(L) - 1)) {
 			break;
 		}
-	}	
-	
+	}
+
 	//Free all memory
-	for(int i = 0; i < line_amount; i++) {
+	for (int i = 0; i < line_amount; i++) {
 		free(strings[i]);
 	}
 	free(strings);
--- pa1/List.h
+++ pa1/List.h
@@ -1,8 +1,9 @@
-//-----------------------------------------------------------------------------
-// List.h
-// Header file for List ADT
-//-----------------------------------------------------------------------------
-
+/*********************************************************************************
+* Brian Nguyen, bnguy118
+* 2022 Winter CSE101 PA1
+* List.h
+* Header file for List ADT
+*********************************************************************************/
 #pragma once
 
 #include <stdbool.h>
@@ -13,88 +14,117 @@ typedef struct ListObj* List;
 
 // Constructors-Destructors ---------------------------------------------------
 
+// newList()
 // Creates and returns a new empty List.
 List newList(void);
 
+// freeList()
 // Frees all heap memory associated with *pL, and sets *pL to NULL.
 void freeList(List* pL);
 
 // Access functions -----------------------------------------------------------
 
+// length()
 // Returns the number of elements in L.
 int length(List L);
 
+// index()
 // Returns index of cursor element if defined, -1 otherwise.
 int index(List L);
 
-// Returns front element of L. Pre: length()>0
+// front()
+// Returns front element of L.
+// Pre: length()>0
 int front(List L);
 
-// Returns back element of L. Pre: length()>0
+// back()
+// Returns back element of L.
+// Pre: length()>0
 int back(List L);
 
-// Returns cursor element of L. Pre: length()>0, index()>=0
+// get()
+// Returns cursor element of L.
+// Pre: length()>0, index()>=0
 int get(List L);
 
-// Returns true iff Lists A and B are in same state, and returns false
-// otherwise.
+// equals()
+// Returns true iff Lists A and B are in same state, and returns false otherwise.
 bool equals(List A, List B);
 
 // Manipulation procedures ----------------------------------------------------
 
+// clear()
 // Resets L to its original empty state.
 void clear(List L);
 
-// Overwrites the cursor element’s data with x. Pre: length()>0, index()>=0
+// set()
+// Overwrites the cursor element’s data with x.
+// Pre: length()>0, index()>=0
 void set(List L, int x);
 
-// If L is non-empty, sets cursor under the front element, otherwise does
-// nothing.
+// moveFront()
+// If L is non-empty, sets cursor under the front element, otherwise does nothing.
 void moveFront(List L);
 
-// If L is non-empty, sets cursor under the back element, otherwise does
-// nothing.
+// moveBack()
+// If L is non-empty, sets cursor under the back element, otherwise does nothing.
 void moveBack(List L);
 
+// movePrev()
 // If cursor is defined and not at front, move cursor one step toward the front
 // of L; if cursor is defined and at front, cursor becomes undefined; if cursor
 // is undefined do nothing.
 void movePrev(List L);
 
+// moveNext()
 // If cursor is defined and not at back, move cursor one step toward the back of
 // L; if cursor is defined and at back, cursor becomes undefined; if cursor is
 // undefined do nothing.
 void moveNext(List L);
 
+// prepend()
 // Insert new element into L. If L is non-empty, insertion takes place before
 // front element.
 void prepend(List L, int x);
 
+// append()
 // Insert new element into L. If L is non-empty, insertion takes place after
 // back element.
 void append(List L, int x);
 
-// Insert new element before cursor. Pre: length()>0, index()>=0
+// insertBefore()
+// Insert new element before cursor.
+// Pre: length()>0, index()>=0
 void insertBefore(List L, int x);
 
-// Insert new element after cursor. Pre: length()>0, index()>=0
+// insertAfter()
+// Insert new element after cursor.
+// Pre: length()>0, index()>=0
 void insertAfter(List L, int x);
 
-// Delete the front element. Pre: length()>0
+// deleteFront()
+// Delete the front element.
+// Pre: length()>0
 void deleteFront(List L);
 
-// Delete the back element. Pre: length()>0
+// deleteBack()
+// Delete the back element.
+// Pre: length()>0
 void deleteBack(List L);
 
-// Delete cursor element, making cursor undefined. Pre: length()>0, index()>=0
+// delete()
+// Delete cursor element, making cursor undefined.
+// Pre: length()>0, index()>=0
 void delete (List L);
 
 // Other operations -----------------------------------------------------------
 
+// printList()
 // Prints to the file pointed to by out, a string representation of L consisting
 // of a space separated sequence of integers, with front on left.
 void printList(FILE* out, List L);
 
+// copyList()
 // Returns a new List representing the same integer sequence as L. The cursor in
 // the new list is undefined, regardless of the state of the cursor in L. The
 // state of L is unchanged.
