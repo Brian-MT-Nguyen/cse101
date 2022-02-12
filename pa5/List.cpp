bc81d05e0463ce1bdcb5cc651f50e9d8dbad8d48
bnguy118 <bnguy118@ucsc.edu>
2022-02-12 00:16:08 -0800

    updating List.cpp accessor

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -59,3 +59,66 @@ List::~List() {
 		eraseAfter();
 	}
 }
+
+// Access functions --------------------------------------------------------
+
+// length()
+// Returns the length of this List.
+int List::length() const {
+	return num_elements;
+}
+
+// front()
+// Returns the front element in this List.
+// pre: length()>0
+ListElement List::front() const {
+	if(num_elements == 0) {
+		throw std::length_error("List: front(): empty List");
+	}
+	return frontDummy->next->data;
+}
+
+// back()
+// Returns the back element in this List.
+// pre: length()>0
+ListElement List::back() const {
+	if(num_elements == 0) {
+		throw std::length_error("List: back(): empty List");
+	}
+	return backDummy->prev->data;
+}
+
+// position()
+// Returns the position of cursor in this List: 0 <= position() <= length().
+int List::position() const {
+	return pos_cursor;
+}
+
+// peekNext()
+// Returns the element after the cursor.
+// pre: position()<length()
+ListElement List::peekNext() const {
+	if(pos_cursor == num_elements) {
+		throw std::length_error("List: peekNext(): out of range");
+	}
+	return afterCursor->data;
+}
+
+// peekPrev()
+// Returns the element before the cursor.
+// pre: position()>0
+ListElement List::peekPrev() const {
+	if(pos_cursor == 0) {
+		throw std::length_error("List: peekPrev(): out of range");
+	}
+	return beforeCursor->data;
+}
+
+// Manipulation procedures -------------------------------------------------
+
+// clear()
+// Deletes all elements in this List, setting it to the empty state.
+void List::clear() {
+
+}
+
