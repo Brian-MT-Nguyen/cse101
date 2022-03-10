1ba7c4949f042fbc8e28e6927012fe8ed949b190
Brian Nguyen <bnguy118@ucsc.edu>
2022-03-10 09:38:24 +0000

    Updating DictionaryTest.cpp header

--- pa8/DictionaryTest.cpp
+++ pa8/DictionaryTest.cpp
@@ -1,7 +1,9 @@
-//-----------------------------------------------------------------------------
-// DictionaryClient.cpp
-// A test client for the Dictionary ADT
-//-----------------------------------------------------------------------------
+/*********************************************************************************
+ * Brian Nguyen, bnguy118
+ * 2022 Winter CSE101 PA8
+ * DictionaryTest.cpp
+ * A test client for the Dictionary ADT
+ *********************************************************************************/
 #include<iostream>
 #include<string>
 #include<stdexcept>
